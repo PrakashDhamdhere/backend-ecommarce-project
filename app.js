2b7bd872ae9413c2e0d60a3f892bbadac469a34e
user.email : "prakashdhamdhere7350@gmail.com <user.email : prakashdhamdhere7350@gmail.com>
2026-01-10 21:14:08 +0530

    intitial stage

---
+++ app.js
@@ -0,0 +1,17 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.set('view engine', 'ejs')
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res)=>{
+    res.render('index');
+    
+});
+
+app.listen(3000);
