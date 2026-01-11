5ed0a4bc373e1cdd753fb7e217e08b9281b47be2
user.email : "prakashdhamdhere7350@gmail.com <user.email : prakashdhamdhere7350@gmail.com>
2026-01-11 18:00:50 +0530

    added separation of concerns

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
 const path = require('path')
+const db = require('./config/mongoose-connection')
+const ownersRouter = require("./routes/ownersRouter")
+const usersRouter = require("./routes/usersRouter")
+const productsRouter = require("./routes/productsRouter")
 
 app.set('view engine', 'ejs')
 app.use(cookieParser())
@@ -9,9 +13,8 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res)=>{
-    res.render('index');
-    
-});
+app.use("/owners", ownersRouter);
+app.use("/users", usersRouter);
+app.use("/products", productsRouter);
 
 app.listen(3000);
