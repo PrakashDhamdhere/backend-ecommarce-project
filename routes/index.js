2adc7830ff551409e5c51569081321eeca0c5f5f
user.email : "prakashdhamdhere7350@gmail.com <user.email : prakashdhamdhere7350@gmail.com>
2026-02-05 13:12:41 +0530

    Admin pannel added to add products

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const isLoggedIn = require('../middlewares/isLoggedIn')
+const productModel = require('../models/product-model')
 
 router.get("/", (req, res)=>{
     let error = req.flash("error");
     let success = req.flash("success")
-    res.render('index', {error, success});
+    res.render('index', {error, success, loginPage: false});
 });
 
-router.get("/shop", (req, res)=>{
-    res.render('shop', {products: []})
+router.get("/shop", isLoggedIn, async (req, res)=>{
+    let products = await productModel.find();
+    res.render('shop', {products})
+})
+
+router.get("/cart", (req, res)=>{
+    res.render('cart')
+})
+
+router.get("/addtocart/:id", isLoggedIn, async (req, res)=>{
+    let products = await productModel.find();
+    res.render('shop', {products})
 })
 
 module.exports = router;
--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -1,9 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const isLoggedIn = require('../middlewares/isLoggedIn')
+const upload = require('../config/multer-config')
+const productModel = require('../models/product-model')
 
 router.get("/", isLoggedIn, (req, res)=>{
     res.send(`Hey product\nYour Name is: ${req.user.fullname}`)
+})
+
+router.post("/create", upload.single('image'), async (req, res)=>{
+    try {
+        let {name, price, discount, bgcolor, panelcolor, textcolor} = req.body;
+        function addHashIfNotExists(str) {
+            if (!str.startsWith("#")) {
+                return "#" + str;
+            }
+            return str;
+        }
+        let product = await productModel.create({
+            image: req.file.buffer,
+            name,
+            price,
+            discount,
+            bgcolor: addHashIfNotExists(bgcolor),
+            panelcolor:addHashIfNotExists(panelcolor),
+            textcolor: addHashIfNotExists(textcolor)
+        })
+        req.flash("success", "Product Created Successfully.")
+        res.redirect("/owners/admin")
+    } catch (error) {
+        res.send(error.message)
+    }
 })
 
 module.exports = router
