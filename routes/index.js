513b85733268ff098a29ab1cf22182fec619d3f9
user.email : "prakashdhamdhere7350@gmail.com <user.email : prakashdhamdhere7350@gmail.com>
2026-02-05 15:18:00 +0530

    cart feature added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const isLoggedIn = require('../middlewares/isLoggedIn')
 const productModel = require('../models/product-model')
+const userModel = require('../models/user-model')
 
 router.get("/", (req, res)=>{
     let error = req.flash("error");
@@ -11,11 +12,34 @@ router.get("/", (req, res)=>{
 
 router.get("/shop", isLoggedIn, async (req, res)=>{
     let products = await productModel.find();
-    res.render('shop', {products})
+    let success = req.flash("success")
+    res.render('shop', {products, success})
 })
 
-router.get("/cart", (req, res)=>{
-    res.render('cart')
+router.get("/addtocart/:productId", isLoggedIn, async (req, res)=>{
+    let user = await userModel.findOne({email: req.user.email})
+    user.cart.push(req.params.productId)
+    await user.save()
+    req.flash("success", "Product added to Cart")
+    res.redirect('/shop')
+})
+router.get("/removefromcart/:productId", isLoggedIn, async (req, res)=>{
+    let user = await userModel.findOne({email: req.user.email})
+    user.cart.splice(user.cart.indexOf(req.params.productId),1)
+    await user.save()
+    req.flash("success", "Product removed from Cart")
+    res.redirect('/cart')
+})
+
+router.get("/cart", isLoggedIn, async (req, res)=>{
+    let user = await userModel
+    .findOne({email: req.user.email})
+    .populate("cart");
+    let totalMRP = 0;
+    user.cart.forEach((val)=>{
+        totalMRP = totalMRP + val.price;
+    })
+    res.render('cart', {user, totalMRP})
 })
 
 router.get("/addtocart/:id", isLoggedIn, async (req, res)=>{
